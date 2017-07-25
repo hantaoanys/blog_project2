66b2135384c71eca6f98f062605902c29e146350
hantao <hantaoanys@163.com>
2017-07-25 11:05:51 +0800

    test1

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,25 @@
+#coding=utf-8
+'''
+Created on 
+
+@author: HanTao
+'''
+from django.db.models.aggregates import Count
+from ..models import Post, Category
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def get_recent_posts(num=5):
+    return Post.objects.all().order_by('-created_time')[:num]
+
+@register.simple_tag
+def archives():
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+@register.simple_tag
+def get_categories():
+    # 别忘了在顶部引入 Category 类
+    category_list = Category.objects.annotate(num_posts=Count('post'))
+    return category_list
---
+++ blog/views.py
@@ -0,0 +1,52 @@
+#coding=utf-8
+import markdown
+from comments.forms import CommentForm
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post,Category
+from django.shortcuts import render, get_object_or_404
+# Create your views here.
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    
+    return render(request,'blog/index.html',context={
+                                    'post_list': post_list,
+        
+        })
+def detail(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
+
+def archives(request,year,month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    #return render(request,'blog/index.html',context={'post':post_list})
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request,pk):
+    cate=get_object_or_404(Category,pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+    
+    if not q:
+        error_msg = '请输入关键词'
+        return render(request,'blog/index.html',{'error_msg':error_msg})
+    post_list = Post.objects.filter(title__icontains=q)
+    return render(request,'blog/index.html',{'error_msg': error_msg,
+                                               'post_list': post_list})
